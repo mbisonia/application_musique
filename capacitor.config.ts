48c02e6b41e9732e28a5c07add51ce4d806ea734
Sonia <mbisonia64@gmail.com>
2022-12-01 18:06:07 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'musique promo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
